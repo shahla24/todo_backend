7052f9f2e42d66d8627862305c61c7c7070e8874
Shahla Atiq <shahlatiq24@gmail.com>
2019-11-26 13:37:37 +0000

    shahlas

--- handler.js
+++ handler.js
@@ -11,8 +11,18 @@ app.use (bodyparser.json());
 
 
 app.get("/tasks", function (request, response) {
-  //do the logic for getting all the tasks from the DB
-  response.status(200).send("Getting all the tasks! yay!");
+  
+  response.status(200).send({
+tasks: [
+{id: 0001, taskDescription: "Distribution of catalogue", completed: true, dateCreated:"2019-11-25"},
+{id: 0002, taskDescription: "Collection of catalogue", completed: true, dateCreated:"2019-11-23"},
+{id: 0003, taskDescription: "Update and submit order", completed: true, dateCreated:"2019-11-20"},
+{id: 0002, taskDescription: "Prepare individual order", completed: true, dateCreated:"2019-11-10"},
+{id: 0002, taskDescription: "Pay the invoice", completed: true, dateCreated:"2019-11-08"},
+{id: 0002, taskDescription: "Check calender dates", completed: true, dateCreated:"2019-11-05"},
+
+]
+  });
 });
 
 app.post("/tasks", function (request, response) {
@@ -24,13 +34,13 @@ app.post("/tasks", function (request, response) {
 
 app.delete("/tasks/:taskId", function (request, response) {
   const taskId = request.params.taskId;
-  response.status(200).send("Deleted task with id " + taskId);
+  response.status(200).send("Received request to delete task with id " + taskId);
 });
 
 app.put("/tasks/:taskId", function (request, response) {
   const taskId = request.params.taskId;
   const updatedTask = request.body;
-  response.status(200).send("Updated task with id " + taskId);
+  response.status(200).send("Updated task with id " + taskId + "with" + task.text + task.completed);
 });
 
 
